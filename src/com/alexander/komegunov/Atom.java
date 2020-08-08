c0f984ff241121b35ae66705fff36cc7d4a177cc
fishand4ips <alekomegunov@gmail.com>
2020-08-08 16:44:01 +0300

    initial commit

---
+++ src/com/alexander/komegunov/Atom.java
@@ -0,0 +1,15 @@
+package com.alexander.komegunov;
+
+public class Atom {
+
+    public static void main( String[] args ) {
+
+        Nucleo nucleo1 = new Nucleo("Sasha", 5,1);
+        Nucleo nucleo2 = new Nucleo("Fedor", 5,10);
+        Nucleo nucleo3 = new Nucleo("Misha", 5,100);
+
+        nucleo1.start();
+        nucleo2.start();
+        nucleo3.start();
+    }
+}
---
+++ src/com/alexander/komegunov/Nucleo.java
@@ -0,0 +1,30 @@
+package com.alexander.komegunov;
+
+public class Nucleo extends Thread {
+
+    private final String name;
+    private final int interval;
+    private final int total;
+
+    public Nucleo(String name, int interval, int total) {
+        this.name = name;
+        this.interval = interval;
+        this.total = total;
+    }
+
+    public void run() {
+        try {
+            for (int i = 1; i <= total; i++) {
+                print(i);
+                Thread.sleep(1000 * interval);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void print(int i) {
+        System.out.println("Name: " + name + " " + i);
+    }
+
+}
